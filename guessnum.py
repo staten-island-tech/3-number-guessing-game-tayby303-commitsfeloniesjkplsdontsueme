efad8ae16591e491216295f87380b349c57c2b32
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-02-11 10:15:53 -0500

    commit

--- guessnum.py
+++ guessnum.py
@@ -1,5 +1,11 @@
 import random
-random=random.randint(1,100)
-num(input("Guess the number 1-100: "))
-while num!=random
-  num(input("Guess again: "))
+random=int(random.randint(1,3))
+num=int(input("Guess the number 1-100: "))
+if num!=random:
+  while num!=random:
+    if num>random:
+      num=int(input("Lower: "))
+    else:
+      num=int(input("Higher: "))
+else:
+  print("Correct")
