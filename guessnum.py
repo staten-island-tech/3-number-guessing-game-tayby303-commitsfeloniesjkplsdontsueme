2c4dee16f8ef98ceded2bbb7c3a85ea01abec6ca
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-02-11 15:29:03 -0500

    Update guessnum.py

--- guessnum.py
+++ guessnum.py
@@ -1,11 +1,10 @@
 import random
-random=int(random.randint(1,3))
+random=random.randint(1,100)
 num=int(input("Guess the number 1-100: "))
-if num!=random:
-  while num!=random:
-    if num>random:
-      num=int(input("Lower: "))
-    else:
-      num=int(input("Higher: "))
-else:
+while num!=random:
+  if num>random:
+    num=int(input("Lower: "))
+  elif num<random:
+    num=int(input("Higher: "))
+if num==random:
   print("Correct")
